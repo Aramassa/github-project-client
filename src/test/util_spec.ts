6807181055ef4e147edd61743964f9185eb27fee
Aramassa <aramassa31@gmail.com>
2020-08-01 22:35:30 +0900

    add test case calculate and remove.

--- src/test/util_spec.ts
+++ src/test/util_spec.ts
@@ -1,11 +1,25 @@
 import {DueStamp} from "../lib/util/DueStamp";
+import { GithubIssue } from "../../dist/lib/github/GithubIssue";
 
-describe("GithubApi", function() {
-  test('update issue', async function(){
-    const issueNumber:number = 3;
+describe("util", ()=>{
+  describe("DueStamp", ()=>{
+    test('modiry title', ()=>{
+      let title:string = "test";
+  
+      expect(DueStamp.modify(title, '2020.03.25')).toEqual("test DUE:(2020.03.25)");
+    });
 
-    let title:string = "test";
+    test('caluculate due', ()=>{
+      let title:string = "test DUE:(2020.03.25)";
 
-    expect(DueStamp.modify(title, '2020.03.25')).toEqual("test DUE:(2020.03.25)");
+      expect(DueStamp.calculate(title, 10, 'day')).toEqual("test DUE:(2020.1.11)");
+    });
+
+    test('remove due', ()=>{
+      let title:string = DueStamp.modify("test", "2020.03.25");
+
+      expect(DueStamp.remove(title)).toEqual("test");
+    });
+
   });
-});
+})
