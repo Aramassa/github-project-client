0c0b8db8504f812b6c6a37a5deb0e38d40724f92
Aramassa <aramassa31@gmail.com>
2020-08-01 22:13:50 +0900

    change indent to 2 spaces.

--- src/test/api_spec.ts
+++ src/test/api_spec.ts
@@ -5,39 +5,38 @@ const apiTest1:GithubApi = new GithubApi("Aramassa", "github-project-due-test");
 
 describe("GithubApi", function() {
 
-    test('list projects on repo', async ()=>{
-        let projects: any = await apiTest1.listProjects();
-        return expect(projects.length).toBeGreaterThanOrEqual(1);
+  test('list projects on repo', async ()=>{
+    let projects: any = await apiTest1.listProjects();
+    return expect(projects.length).toBeGreaterThanOrEqual(1);
+  });
+
+  test('get repository issue list', async ()=>{
+    let issue: any = await apiTest1.getRepoIssueList();
+    return expect(issue.length).toBeGreaterThanOrEqual(1);
+  });
+
+  test('get repository issue by id', async ()=>{
+    let issue: any = await apiTest1.getIssue(1);
+  })
+
+  test('list cards on project', async ()=>{
+    let cards = await apiTest1.listProjectCards(projectId);
+  });
+
+  test('list columns on project', async ()=>{
+    let columns = await apiTest1.listProjectColumns(projectId);
+  });
+
+  test('comment to issue', async ()=>{
+    let issue: any = await apiTest1.getRepoIssueList();
+  });
+
+  test('update issue', async function(){
+    const issueNumber:number = 3;
+
+    let issue: any = await apiTest1.getIssue(issueNumber);
+    await apiTest1.editIssue(issueNumber, {
+      title: issue.title + ' 2020.7.26'
     });
-
-    test('get repository issue list', async ()=>{
-        let issue: any = await apiTest1.getRepoIssueList();
-        return expect(issue.length).toBeGreaterThanOrEqual(1);
-    });
-
-    test('get repository issue by id', async ()=>{
-        let issue: any = await apiTest1.getIssue(1);
-    })
-
-    test('list cards on project', async ()=>{
-
-        let cards = await apiTest1.listProjectCards(projectId);
-    });
-
-    test('list columns on project', async ()=>{
-        let columns = await apiTest1.listProjectColumns(projectId);
-    });
-
-    test('comment to issue', async ()=>{
-        let issue: any = await apiTest1.getRepoIssueList();
-    });
-
-    test('update issue', async function(){
-        const issueNumber:number = 3;
-
-        let issue: any = await apiTest1.getIssue(issueNumber);
-        await apiTest1.editIssue(issueNumber, {
-            title: issue.title + ' 2020.7.26'
-        });
-    });
+  });
 });
